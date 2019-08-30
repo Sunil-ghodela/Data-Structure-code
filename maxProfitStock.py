90f92bf82986d5b9935a5633738491fe483debcd
Sunil-ghodela <35575602+Sunil-ghodela@users.noreply.github.com>
2019-08-30 13:49:45 +0530

    Update maxProfitStock.py

--- maxProfitStock.py
+++ maxProfitStock.py
@@ -1,6 +1,3 @@
-minVal = 0
-maxVal = 10
-
 stockArray = [100, 180, 260, 310, 40, 535, 695]
 
 def maxProfitAaja(stockList):
@@ -13,28 +10,19 @@ def maxProfitAaja(stockList):
     tempStockDiff = 0
 
     if stockBuy == None:
-      if stockList[start] < stockList[start+1]:
-        stockBuy = stockList[start]
+      if stockList[start] > stockList[start+1]:
+        stockBuy = stockList[start+1]
     else:
-      if start+1 < len(stockList):
-        if stockList[start] > stockList[start-1]:
-          # print(start)
-          # print(stockList[start])
-          # print(stockList[start+1])
-          # print(stockList[start] - stockList[start-1])
-          tempStockDiff = stockList[start] - stockList[start-1]
-        else:
-          # print(start)
-          # print(tempStockDiff)
-          # print(stockBuy)
-          stockProfitList.append({ 'stock buy'+ str(stockBuy): 'stock sell' + str(stockList[start-1])})
+      if start+1 <= len(stockList):
+        if start+1 == len(stockList):
+          stockProfitList.append({ 'stock buy'+ str(stockBuy): 'stock sell' + str(stockList[start])})
           stockBuy = None
-
-
-          # if tempStockDiffPlusOne > tempStockDiff:
-          #   tempStockDiff = tempStockDiffPlusOne
-          # else:
-          #   stockProfitList.append({stockBuy:stockList[start]})
+        else:
+          if stockList[start] < stockList[start+1]:
+            tempStockDiff = stockList[start] - stockList[start-1]
+          else:
+            stockProfitList.append({ 'stock buy'+ str(stockBuy): 'stock sell' + str(stockList[start-1])})
+            stockBuy = None
     start += 1
   
   return stockProfitList
