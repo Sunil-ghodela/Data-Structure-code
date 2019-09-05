2caa37b704ac9ef0bc8d39535742164fce19995b
Sunil Ghodela <ss.sunil9255@gmail.com>
2019-09-05 13:04:03 +0530

    file change

--- maxProfitComplexN.py
+++ maxProfitComplexN.py
@@ -1,6 +1,6 @@
 # stockArray = [100, 1, 1000, 100, 200, 310, 40, 535, 6, 10000]
-stockArray = [100, 6, 1000, 100, 200, 310, 40, 100, 535, 10, 10000]
-# stockArray = [100, 1, 1000, 100, 200, 310, 40, 100, 535, 60, 10000]
+# stockArray = [100, 6, 1000, 100, 200, 310, 40, 100, 535, 10, 10]
+stockArray = [100, 1, 1000, 100, 200, 310, 40, 100, 535, 60, 10000]
 
 
 def maxProfitAaja(stockList):
@@ -14,20 +14,27 @@ def maxProfitAaja(stockList):
         if start+1 <= len(stockList)-1:
             if stockList[start+1] - stockList[start] > tempStockDiff:
                 tempStockDiff = stockList[start+1] - stockList[start]
-                stockProfitList[stockList[start]] = stockList[start+1] - stockList[start]
+                if stockProfitList != {}:
+                    for item in stockProfitList.keys():
+                        if stockProfitList[item] < (stockList[start+1] - stockList[start]):
+                            del stockProfitList[item]
+                            stockProfitList[stockList[start]] = stockList[start+1]
+                else:
+                    stockProfitList[stockList[start]] = stockList[start+1]
 
 
-        # checking last max. score if any then updating it
-        for temp in range(len(stockList)):
-            if stockList[temp] in stockProfitList:
-                if stockProfitList[stockList[temp]] < tempStockDiff:
-                    del stockProfitList[stockList[temp]]
-                    stockProfitList[stockList[start]] = tempStockDiff
+
+        # # checking last max. score if any then updating it
+        # for temp in range(len(stockList)):
+        #     if stockList[temp] in stockProfitList:
+        #         if stockProfitList[stockList[temp]] < tempStockDiff:
+        #             del stockProfitList[stockList[temp]]
+        #             stockProfitList[stockList[start]] = tempStockDiff
 
         start += 1
 
-    stockProfitList[stockProfitList.keys()[0]] = stockProfitList.keys()[
-        0] + stockProfitList[stockProfitList.keys()[0]]
+    # stockProfitList[stockProfitList.keys()[0]] = stockProfitList.keys()[
+    #     0] + stockProfitList[stockProfitList.keys()[0]]
 
     return stockProfitList
 
